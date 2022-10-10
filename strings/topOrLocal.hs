2d9e5c252f798f6bbe831612afa98a0a169d44d9
Yuval K <46052720+supraBlatt@users.noreply.github.com>
2022-10-10 05:30:13 +0300

    hi

--- strings/topOrLocal.hs
+++ strings/topOrLocal.hs
@@ -21,3 +21,24 @@ area2 d = pi * r ^ 2
 
 -- >>> :t concat
 -- concat :: Foldable t => t [a] -> [a]
+
+-- >>> :i Foldable
+-- type Foldable :: (* -> *) -> Constraint
+-- class Foldable t where
+--   fold :: Monoid m => t m -> m
+--   foldMap :: Monoid m => (a -> m) -> t a -> m
+--   foldMap' :: Monoid m => (a -> m) -> t a -> m
+--   foldr :: (a -> b -> b) -> b -> t a -> b
+--   foldr' :: (a -> b -> b) -> b -> t a -> b
+--   foldl :: (b -> a -> b) -> b -> t a -> b
+--   foldl' :: (b -> a -> b) -> b -> t a -> b
+--   foldr1 :: (a -> a -> a) -> t a -> a
+--   foldl1 :: (a -> a -> a) -> t a -> a
+--   toList :: t a -> [a]
+--   null :: t a -> Bool
+--   length :: t a -> Int
+--   elem :: Eq a => a -> t a -> Bool
+--   maximum :: Ord a => t a -> a
+--   minimum :: Ord a => t a -> a
+--   sum :: Num a => t a -> a
+--   product :: Num a => t a -> a
