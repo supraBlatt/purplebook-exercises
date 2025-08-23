768ad43ed0722e97c63fcf1d5eea638c8f898258
Yuval <katyu0729@gmail.com>
2025-08-24 02:19:58 +0300

    hi im back:tm:

--- 08_recursion/08_recursion.hs
+++ 08_recursion/08_recursion.hs
@@ -23,11 +23,22 @@ multInt :: (Integral a) => a -> a -> a
 multInt a 0 = 0
 multInt a b = a + multInt a (b - 1)
 
+applyTimes f 0 = id
+applyTimes f n = f . applyTimes f (n - 1)
+
+multInt1 :: (Eq t, Num t, Num b) => b -> t -> b
+multInt1 a b = applyTimes (+a) b 0
+
+-- >>> multInt1 4 5
+-- 20
+
+multInt' :: (Eq t1, Num t1, Num t2) => t2 -> t1 -> t2
 multInt' a b = go a b 0
   where
-    go a b cur
-      | b == 0 = cur
-      | otherwise = go a (b - 1) (cur + a)
+    go a 0 cur = cur
+    go a b cur = go a (b - 1) (cur + a)
+      -- | b == 0 = cur
+      -- | otherwise = go a (b - 1) (cur + a)
 
 -- until i find a better way to write this..
 multIntWrap :: (Integral a) => a -> a -> a
@@ -36,10 +47,10 @@ multIntWrap a b
   | otherwise = multInt' b a
 
 -- >>> multInt 0 100000
--- 100000
+-- 0
 
--- >>> multInt' 4 10
--- 40
+-- >>> multInt' 4 11
+-- 44
 
 mcCarthy x 
     | x > 100 = x - 10 
--- 08_recursion/integral_div.hs
+++ 08_recursion/integral_div.hs
@@ -7,14 +7,23 @@ dividedBy num denom = go num denom 0
             | n < d = (count, n)
             | otherwise = go (n - d) d (count + 1)
 
+-- crude attempt to add division by zero. negatives not supported.
+dividedBy' :: (Integral a) => a -> a -> DividedResult a
+dividedBy' num denom = go num denom 0
+    where
+        go n d count
+            | d == 0 = DividedByZero
+            | n < d = Result (count, n)
+            | otherwise = go (n - d) d (count + 1)
+
+data DividedResult a = Result (a, a) | DividedByZero deriving Show
+
 -- >>> dividedBy 9 7
 -- (1,2)
+
+-- >>>  dividedBy' (-10) 2
+-- Result (0,-10)
 
 -- >>> dividedBy 4 0
 -- slowed my computer to a halt nice, didn't show bottom
 -- >>> :t dividedBy (abs 1) (abs -3)
-
-mcCarthy x 
-    | x > 100 = x - 10 
-    | otherwise = mcCarthy . mcCarthy $ x + 11 
-
